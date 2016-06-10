16c93551c520f6b7b36805431f582d77389b128d
Srav6924 <sravanthi6924@gmail.com>
2016-06-09 20:47:02 -0400

    my commit

--- testapp/config/deploy.rb
+++ testapp/config/deploy.rb
@@ -5,6 +5,26 @@
 # Simply add the code below to config/deploy.rb, then run 'cap deploy:push' to
 # test it, and 'cap deploy' to deploy as usual.
 
+lock '3.3.5'
+
+set :application, 'spui'
+set :repo_url, 'git@github.com:MYGITHUB/MYAPP.git'
+
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+set :use_sudo, false
+set :bundle_binstubs, nil
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:reload'
+  end
+end
+
 namespace :deploy do
   desc "Push local changes to Git repository"
   task :push do
